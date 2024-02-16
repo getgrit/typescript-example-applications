d5825b4388696c7cff0db4d012bea4c1baa1d306
Morgante Pell <morgantep@google.com>
2024-02-15 23:51:15 -0500

    add workflow

---
+++ .grit/workflows/upgrade_workos.sh
@@ -0,0 +1,2 @@
+/Users/morgante/code/grit/rewriter/target/release/marzano apply 'language json upgrade_js_dependency(target_dep="@workos-inc/node", target_version="6.0.0")' --force
+/Users/morgante/code/grit/rewriter/target/release/marzano apply workos_v6 --force
